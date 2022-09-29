87aecfce94ba4c16d4d8788c409817c7ad82f023
Shubhasish <shubhasishb10@gmail.com>
2022-09-29 19:18:30 +0530

    Refactored to multi module application

---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Owner.java
@@ -0,0 +1,6 @@
+package guru.springframework.petclinic.model;
+
+public class Owner extends Person {
+
+
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/petclinic/model/Pet.java
@@ -0,0 +1,43 @@
+package guru.springframework.petclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private String name;
+    private Owner owner;
+    private PetType petType;
+    private LocalDate dateOfBirth;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public PetType getPetType() {
+        return petType;
+    }
+
+    public void setPetType(PetType petType) {
+        this.petType = petType;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+}
